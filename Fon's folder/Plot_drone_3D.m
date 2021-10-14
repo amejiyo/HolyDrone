2a52fd0abbb525b06f3f9bb0e92ca9e3d92fd828
imchin <imarkchin@gmail.com>
2021-10-15 00:39:05 +0700

    Add noise,render video

--- Fon's folder/Plot_drone_3D.m
+++ Fon's folder/Plot_drone_3D.m
@@ -26,6 +26,14 @@ Z=[]
 rotm=[1 0 0; 0 1 0;0 0 1];
 
 
+% fid = figure;
+% writerObj = VideoWriter('Simmulation3.avi'); % Name it.
+% writerObj.FrameRate = 20; % How many frames per second.
+% open(writerObj); 
+
+
+
+
 time=0;
 index=1;
 tic
@@ -43,27 +51,40 @@ while(1)
        hold all
        grid on
        view(3)
-       
+       axis([-5 5 -5 5 0 15])
          
        rotm =  eul2rotm( [realRz(index),realRy(index),realRx(index)]);
        plotDrone([realx(index),realy(index),realz(index)],[0,0,0],0.1,rotm)
        plot3(X,Y,Z,'--','color','black')
-     
+       view(40,40)
+       
+       
        
 %        pause(1);
-       
-       
+
+%         figure(fid); % Makes sure you use your desired frame
+%         frame = getframe(gcf); % 'gcf' can handle if you zoom in to take a movie.
+%         writeVideo(writerObj, frame);
+      
+        
+        
+        
        index=index+1;
    end
+   
+   
    if(index>size(realt,2))
        disp("End of simulation")
        break
    end
    
         
-   
+     
 
    pause(0.01);
    time=toc;
    disp(time)
 end
+
+% hold off
+% close(writerObj); % Saves the movie.
