df3556d7734eb1db26d215e8d60b05f9d2913fbd
nattasit63 <teeteethewacko@gmail.com>
2021-11-17 05:48:00 +0700

    random tune

--- Fon's folder/jacomeasure.m
+++ Fon's folder/jacomeasure.m
@@ -3,15 +3,15 @@ function c = jacomeasure(x)
 %   Detailed explanation goes here
 
 % Jacobian size = 6*9
-% d_r = [ cos(x(9))*sin(x(8))*cos(x(7)) + sin(x(9))*sin(x(6)), cos(x(9))*cos(x(8))*sin(x(7)), -sin(x(9))*sin(x(8))*sin(x(7)) - cos(x(9))*cos(x(7));
-%        sin(x(9))*sin(x(8))*cos(x(7)) - cos(x(9))*sin(x(6)), sin(x(9))*cos(x(8))*sin(x(7)), cos(x(9))*sin(x(8))*sin(x(7)) + sin(x(9))*sin(x(7));
-%        cos(x(8))*cos(x(7))                                , -sin(x(8))*sin(x(7))         , 0];
+d_r = [ cos(x(9))*sin(x(8))*cos(x(7)) + sin(x(9))*sin(x(6)), cos(x(9))*cos(x(8))*sin(x(7)), -sin(x(9))*sin(x(8))*sin(x(7)) - cos(x(9))*cos(x(7));
+       sin(x(9))*sin(x(8))*cos(x(7)) - cos(x(9))*sin(x(6)), sin(x(9))*cos(x(8))*sin(x(7)), cos(x(9))*sin(x(8))*sin(x(7)) + sin(x(9))*sin(x(7));
+       cos(x(8))*cos(x(7))                                , -sin(x(8))*sin(x(7))         , 0];
 
-d_r = [-cos(x(9))*sin(x(7)),sin(x(9))*sin(x(8))+cos(x(9))*sin(x(7))*cos(x(8)),cos(x(9))*sin(x(8))-sin(x(9))*sin(x(7))*cos(x(8));
-        -sin(x(9))*sin(x(7)),-cos(x(9))*sin(x(8))+sin(x(9))*sin(x(7))*cos(x(8)),sin(x(9))*sin(x(8))+cos(x(9))*sin(x(7))*cos(x(8));
-        -cos(x(7)),0,0];
+% d_r = [-cos(x(9))*sin(x(7)),sin(x(9))*sin(x(8))+cos(x(9))*sin(x(7))*cos(x(8)),cos(x(9))*sin(x(8))-sin(x(9))*sin(x(7))*cos(x(8));
+%         -sin(x(9))*sin(x(7)),-cos(x(9))*sin(x(8))+sin(x(9))*sin(x(7))*cos(x(8)),sin(x(9))*sin(x(8))+cos(x(9))*sin(x(7))*cos(x(8));
+%         -cos(x(7)),0,0];
         
 %c=[diag([1;1;1]),zeros(3),zeros(3);zeros(3),zeros(3),d_r];
-c=[diag([1;1;1]),zeros(3),zeros(3);zeros(3),zeros(3),d_r*diag([1;1;1])];
+c=[diag([1;1;1]),zeros(3),zeros(3);zeros(3),zeros(3),d_r];
 end
 
--- Fon's folder/myStateTransitionFcn.m
+++ Fon's folder/myStateTransitionFcn.m
@@ -4,7 +4,7 @@ x1=x(1:3,:);
 x2=x(4:6,:);
 r =x(7:9,:);
 
-x1k=x1+x2.*deltat + 0.5*deltat^2*(x2.*0.001);
+x1k=x1+x2.*deltat ;
 J=[1,sin(r(1))*tan(r(2)),cos(r(1))*tan(r(2)); 0,cos(r(1)),-sin(r(1)); 0,sin(r(1))/cos(r(2)),cos(r(1))/cos(r(2)) ];
 x2k=x2;
 x3k=r+J*x1.*deltat;
