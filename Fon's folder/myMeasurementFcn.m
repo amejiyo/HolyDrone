d0441f2c6204919eeb108d0b748a91d0167c66cc
nattasit63 <teeteethewacko@gmail.com>
2021-11-17 18:13:11 +0700

    mark update

--- Fon's folder/myMeasurementFcn.m
+++ Fon's folder/myMeasurementFcn.m
@@ -1,7 +1,7 @@
 function y = myMeasurementFcn(x)
-x3 = roty(x(8))*rotx(x(7))*rotz(x(9))*[0;1;0];
+%x3 = roty(x(7))*rotx(x(8))*rotz(x(9))*[0;1;0];
 
-%x3 = eul2rotm(x(7:9)')*[0;1;0];
+x3 = eul2rotm(x(7:9)')*[0;1;0];  %zyx
 y=[x(1:3);x3];
 end
 
